d6a2fc547bd6a6203064ae63de877bb84b3fd55e
SAJacob7 <sophiajacob@ku.edu>
2025-10-24 01:48:47 -0500

    new babel and cleaned code

--- Elysian/babel.config.js
+++ Elysian/babel.config.js
@@ -1,15 +1,15 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['module:metro-react-native-babel-preset'],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module:react-native-dotenv',
         {
-          moduleName: '@env',      // import from '@env'
-          path: '.env',            // path to your .env file
-          safe: false,             // set true if you have a .env.example
-          allowUndefined: true,    // don’t throw if some variables are missing
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
         },
       ],
     ],
