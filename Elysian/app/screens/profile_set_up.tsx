1a33c1ced7f44ba8d3172f41b984fb31901b73fc
Nikka Vuong <nikkavuong@gmail.com>
2025-10-24 14:49:18 -0500

    Add array of questions.

--- Elysian/app/screens/profile_set_up.tsx
+++ Elysian/app/screens/profile_set_up.tsx
@@ -11,9 +11,18 @@ export type RootParamList = {
 };
 const profileSetUp = () => {
   const navigation = useNavigation<ProfileSetUpScreenProp>();
+  const questions = [{question: "Where are you traveling from?", answer: []},
+    {question: "What type of vacation are you looking for?", answer: ["Beach", "City", "Historical", "Adventure", "Nature", "Religious"]},
+    {question: "What seasons do you like?", answer: ["Spring", "Summer", "Fall", "Winter"]},
+    {question: "What is your budget?", answer: ["Budget Friendly", "Mid-Range", "Luxury", "Premium"]},
+    {question: "What has been your favorite country you've visted?", answer: []},
+    {question: "How far do you want to travel?", answer: ["Within your Country", "Within your Continent", "Outside of your Continent", "Anywhere"]},
+    {question: "What type of place do you like?", answer: ["Quiet", "Moderate", "Busy"]},
+  ];
   return (
     <View style={styles.container}>
       <Text variant="headlineLarge" style={styles.title}>Profile Set Up Page</Text>
+      <Text variant="headlineMedium" style={styles.title}>Answer the following questions for your reccomendations!</Text>
     </View>
   );
 }
